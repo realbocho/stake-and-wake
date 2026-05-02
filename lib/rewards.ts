6e52319a3ef003df47780e862095924795a7ca23
realbocho <92915087+realbocho@users.noreply.github.com>
2026-05-02 21:25:59 +0900

    Update rewards.ts

--- lib/rewards.ts
+++ lib/rewards.ts
@@ -7,14 +7,9 @@ export function calculatePoolPayout(input: {
   const distributablePool = input.failedStakeTon * (1 - env.platformFeeRate);
   const perWinnerTon =
     input.successCount > 0 ? distributablePool / input.successCount : 0;
-
   return {
     distributablePool,
     platformFeeTon: input.failedStakeTon * env.platformFeeRate,
     perWinnerTon
   };
-}
-
-export function calculateWeeklyPerfectGroupBonus(groupMemberCount: number) {
-  return groupMemberCount >= 10 ? 5 : 0;
 }
